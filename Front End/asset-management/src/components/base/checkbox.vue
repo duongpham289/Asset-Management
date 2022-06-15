14f00fa0f02fc58685a586343e4bcfbcfe1d9b85
minhduc275 <55028284+minhduc275@users.noreply.github.com>
2022-06-16 00:27:33 +0700

    checkbox

---
+++ Front End/asset-management/src/components/base/checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Nếu true thì nhận giá trị css -->
+  <div
+    class="m-checkbox"
+    :class="{
+      'm-checkbox-selected': isChecked,
+    }"
+  ></div>
+</template>
+<script>
+export default {
+  name: "the-checkbox",
+  props: ["checked"],
+  watch: {
+    checked: function (newValue) {
+      this.isChecked = newValue;
+    },
+  },
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+};
+</script>
+<style></style>
