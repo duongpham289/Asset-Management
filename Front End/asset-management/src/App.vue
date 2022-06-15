aa400914208d5a03750b08762b6e4d2e470e8edf
Le Tuan <lethientuan8668@gmail.com>
2022-06-15 20:56:11 +0700

    setup dự án
    - Chia layout
    - Navbar

---
+++ Front End/asset-management/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="m-layout">
+    <TheNavbar></TheNavbar>
+    <div class="m-main">
+      <TheHeader></TheHeader>
+      <TheContent></TheContent>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "./components/layout/TheNavbar.vue";
+import TheContent from "./components/layout/TheContent.vue";
+import TheHeader from "./components/layout/TheHeader.vue";
+export default {
+  name: "App",
+
+  components: {
+    TheNavbar,
+    TheContent,
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url(./css/main.css);
+</style>
---
+++ Front End/asset-management/src/components/layout/TheNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-navbar" style="width: 266px">
+    <div class="m-navbar-header">
+      <div class="navbar-header-logo"></div>
+      <div class="navbar-header-title">QLTS</div>
+    </div>
+
+    <ul class="m-navbar-list">
+      <li class="navbar-item">
+        <div class="navbar-logo"></div>
+        <div class="navbar-item-title">
+          <p>Tài sản</p>
+        </div>
+      </li>
+    </ul>
+
+    <div class="close-navbar">
+      <div class="close-navbar-box" @click="onClickToggleNavBar"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
