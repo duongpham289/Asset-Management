facf1bf8e9883c06d32b339ccf5d279556770b34
minhduc275 <55028284+minhduc275@users.noreply.github.com>
2022-06-16 00:19:51 +0700

    button

---
+++ Front End/asset-management/src/components/base/basebutton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button :disabled="disabled" class="m-button" :class="type" ref="button">
+    {{ buttonTitle }}
+  </button>
+</template>
+<script>
+export default {
+  name: 'the-button',
+  props: ['buttonTitle', 'type', 'disabled', 'isValid'],
+  methods: {
+    setFocus() {
+      this.$refs.button.focus();
+    },
+  },
+};
+</script>
+<style></style>
