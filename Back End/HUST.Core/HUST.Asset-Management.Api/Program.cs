d454e659e43cf19695e4d940441caad17030bc9d
vtnghia187332 <62042890+vtnghia187332@users.noreply.github.com>
2022-06-27 09:58:38 +0700

    config project with CORS and DI

--- Back End/HUST.Core/HUST.Asset-Management.Api/Program.cs
+++ Back End/HUST.Core/HUST.Asset-Management.Api/Program.cs
@@ -1,3 +1,7 @@
+﻿using HUST.Core.Interfaces.Respositories;
+using HUST.Infrastructor.Repository;
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Thêm Policy của CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+
+});
+// Thêm Newtonsoft JSON để sửa lỗi trả về của API
+builder.Services.AddMvc()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+    });
+
+
+// Cấu hình Dependency Injection:
+
+//Repository
+builder.Services.AddScoped<IFixedAssetRepository, FixedAssetRepository>();
+builder.Services.AddScoped<IFixedAssetCategoryRepository, FixedAssetCategoryRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+//Service
+
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
 
 var app = builder.Build();
 
@@ -16,6 +54,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapControllers();
